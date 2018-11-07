7f490251d203d6211038bb17d8b4061a94ccd83b
novakvova <novakvova@gmail.com>
2018-11-07 17:47:17 +0200

    OnClidk and state

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,16 +1,34 @@
-import React from 'react';
-function Article(props)
-{
-    const {arcticle} = props;
-    console.log(props);
-    const body=<section>{arcticle.text}</section>
-    return (
-        <div>
-            <h2>{arcticle.title}</h2>
-            {body}
-            <h3>creation date: {(new Date(arcticle.date)).toDateString()}</h3>
-        </div>
-    )
+import React, { Component } from 'react';
+
+class Article extends Component {
+
+    state = {
+        isOpen: true
+    }
+    render() {
+        const { arcticle } = this.props;
+        console.log(this.props);
+        const body = this.state.isOpen && <section>{arcticle.text}</section>
+        return (
+            <div>
+                <h2>
+                    {arcticle.title}
+                    <button onClick={this.handlerClick}>
+                        {this.state.isOpen ? "close":"open"}
+                    </button>
+                </h2>
+                {body}
+                <h3>creation date: {(new Date(arcticle.date)).toDateString()}</h3>
+            </div>
+        )
+    }
+
+    handlerClick=() => {
+        console.log('------','clicked');
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
 }
 
 export default Article
